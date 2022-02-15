787362292e471d74f88ff4ddd44a05d4adadebf5
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2022-02-15 14:14:22 +0100

    refactor: input style on meeting participant api

--- src/Api/Meetings/MeetingParticipant.php
+++ src/Api/Meetings/MeetingParticipant.php
@@ -7,7 +7,11 @@ use Offlineagency\LaravelWebex\Entities\Meetings\MeetingParticipant as MeetingPa
 
 class MeetingParticipant extends AbstractApi
 {
-    public function list(string $meetingId, ?int $max = null, ?string $hostEmail = null): ?array
+    public function list(
+        string $meetingId,
+        ?int $max = null,
+        ?string $hostEmail = null
+    ): ?array
     {
         $meeting_participants = $this->get('meetingParticipants', [
             'meetingId' => $meetingId,
