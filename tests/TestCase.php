18ac7cf3f1dcee9205d557c4fa08f3a8bd764475
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2022-02-18 17:51:14 +0100

    fix: set config fields on test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,6 +16,18 @@ abstract class TestCase extends BaseTestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('laravel-webex.base_url', 'https://webexapis.com/v1/');
+        $app['config']->set('webex.base_url', 'https://webexapis.com/v1/');
+
+        $app['config']->set('webex.access_token.url', 'access_token');
+        $app['config']->set('webex.access_token.grant_type', 'authorization_code');
+
+        $app['config']->set('webex.refresh_token.url', 'refresh_token');
+        $app['config']->set('webex.refresh_token.grant_type', 'refresh_token');
+
+        $app['config']->set('webex.client.id', '');
+        $app['config']->set('webex.client.secret', '');
+        $app['config']->set('webex.client.code', '');
+
+        $app['config']->set('webex.redirect_uri', '');
     }
 }
